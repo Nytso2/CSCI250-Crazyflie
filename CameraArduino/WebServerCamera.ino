c9e9722d1fba56e85a4074715a1e962bfc96cb5f
Luis Coronel <luiscoronel2500@gmail.com>
2025-05-23 16:26:44 -0400

    Update and rename CameraWebServer.ino to WebServerCamera.ino

--- CameraArduino/WebServerCamera.ino
+++ CameraArduino/WebServerCamera.ino
@@ -1,36 +1,13 @@
+// File name: WebServerCamera.ino
+// Authors : Luis Coronel , Philip Kwan, Saad Ahmed Khan Ghori , Kidus Tegene
+// Description: establishes a connection between a ESP-32 board and whatever network you want it
+// to be, both your device you want to recieve data from and your ESP32 need to be in the same 
+// connection
+
 #include "esp_camera.h"
 #include <WiFi.h>
-
-//
-// WARNING!!! PSRAM IC required for UXGA resolution and high JPEG quality
-//            Ensure ESP32 Wrover Module or other board with PSRAM is selected
-//            Partial images will be transmitted if image exceeds buffer size
-//
-//            You must select partition scheme from the board menu that has at least 3MB APP space.
-//            Face Recognition is DISABLED for ESP32 and ESP32-S2, because it takes up from 15
-//            seconds to process single frame. Face Detection is ENABLED if PSRAM is enabled as well
-
-// ===================
-// Select camera model
-// ===================
-//#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
-//#define CAMERA_MODEL_ESP_EYE  // Has PSRAM
-//#define CAMERA_MODEL_ESP32S3_EYE // Has PSRAM
-//#define CAMERA_MODEL_M5STACK_PSRAM // Has PSRAM
-//#define CAMERA_MODEL_M5STACK_V2_PSRAM // M5Camera version B Has PSRAM
-//#define CAMERA_MODEL_M5STACK_WIDE // Has PSRAM
-//#define CAMERA_MODEL_M5STACK_ESP32CAM // No PSRAM
-//#define CAMERA_MODEL_M5STACK_UNITCAM // No PSRAM
-//#define CAMERA_MODEL_M5STACK_CAMS3_UNIT  // Has PSRAM
+// include the library you want to use, in our case we used CAMERA_MODEL_AI_THINKER
 #define CAMERA_MODEL_AI_THINKER // Has PSRAM
-//#define CAMERA_MODEL_TTGO_T_JOURNAL // No PSRAM
-//#define CAMERA_MODEL_XIAO_ESP32S3 // Has PSRAM
-// ** Espressif Internal Boards **
-//#define CAMERA_MODEL_ESP32_CAM_BOARD
-//#define CAMERA_MODEL_ESP32S2_CAM_BOARD
-//#define CAMERA_MODEL_ESP32S3_CAM_LCD
-//#define CAMERA_MODEL_DFRobot_FireBeetle2_ESP32S3 // Has PSRAM
-//#define CAMERA_MODEL_DFRobot_Romeo_ESP32S3 // Has PSRAM
 #include "camera_pins.h"
 
 // ===========================
@@ -38,10 +15,7 @@
 // ===========================
 const char *ssid = "name";
 const char *password = "password";
-
 // ensure that this connection is under a 2.4 GhZ connection please :>
-
-
 
 void startCameraServer();
 void setupLedFlash(int pin);
